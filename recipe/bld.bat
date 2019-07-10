5afe03bb1a614b8d20466ac90510e0a7478db491
Wolf Vollprecht <w.vollprecht@gmail.com>
2019-07-10 08:47:40 -0500

    MNT: Re-rendered with conda-build 3.18.3, conda-smithy 3.4.0, and conda-forge-pinning 2019.07.04

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,4 @@
-python -m pip install . --no-deps -vv
+%PYTHON% -m pip install . --no-deps -vv
 
 IF NOT EXIST %PREFIX%\condabin GOTO END
 COPY %PREFIX%\bin\mamba.exe %PREFIX\condabin\mamba.exe
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,5 @@
-python -m pip install . --no-deps -vv
+$PYTHON -m pip install . --no-deps -vv
+
 if [ -d "$PREFIX/condabin" ]; then
 	echo "Adding link to mamba into condabin";
 	ln -s $PREFIX/bin/mamba $PREFIX/condabin/mamba
